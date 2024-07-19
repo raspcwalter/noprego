dce5e06958fe3816b90ade1d983382e3beb82982
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-07-19 17:18:21 -0300

    fix: set http settings, render supports TLS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express'
 import cors from 'cors';
-import fs from 'fs';
-import https from 'https';
 import mongoose from 'mongoose';
 import User from './model/User.js';
 import Project from './model/Project.js';
@@ -13,6 +11,13 @@ app.use(express.json());
 
 dotenv.config();
 const PORT = isNaN(parseInt(process.env.PORT)) ? 3000 : parseInt(process.env.PORT); 
+
+class ContractEnum {
+    static ERC1155WRAPPER = 'ERC1155Wrapper.sol';
+    static LOCK = 'Lock.sol';
+    static NOPREGO = 'NoPrego.sol';
+    static NOPREGONFT = 'NoPregoNFT.sol';
+}
 
 //web2
 app.get('/', (req, res) => {
@@ -67,7 +72,6 @@ app.post("/artwork", async (request, response) => {
         response.status(500).json({message: error.message});
     }
 });
-
 
 const getApiKey = async () => {
     const project = await Project.findOne();
@@ -139,22 +143,13 @@ const deployImageIPFS = async (url) => {
 
 //web3
 
-
-
-const options = {
-    key: fs.readFileSync('./localhost-key.pem', 'utf8'),
-    cert: fs.readFileSync('./localhost.pem', 'utf8')
-};
-const httpsServer = https.createServer(options, app);
-
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("MongoDB connection success");
-    httpsServer.listen(PORT, () => {
-        console.log("Server listening at port "+PORT);
-    });
+    app.listen(PORT, function(err){
+        if (err) console.log("Error in server setup");
+        console.log("Server listening on Port", PORT);
+    })
 }).catch(() => {
     console.log("MongoDB connection failed")
-}).catch(() => {
-    console.log("MongoDB connection failed");
 })
